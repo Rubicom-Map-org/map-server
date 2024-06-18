738409f9cb7c3254290f95b689a4b50a8985d00f
yurkenchik <yura.ilchyshyn06@gmail.com>
2024-06-18 10:18:43 +0300

    updated users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,20 @@
-import {Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Req, Res, UseGuards} from '@nestjs/common';
-import {UsersService} from "../services/users.service";
-import {User} from "../entities/users.entity";
-import {AuthGuard} from "../../auth/auth.guard";
+import {Controller, Delete, Get, HttpException, HttpStatus, Param, Req, UseGuards} from '@nestjs/common';
+import {UsersService} from "./users.service";
+import {User} from "./users.entity";
+import {AuthGuard} from "../auth/auth.guard";
 import {ApiOperation, ApiResponse} from "@nestjs/swagger";
-import {GetUserProfileDto} from "../dto/get-user-profile.dto";
-import {Token} from "../entities/tokens.entity";
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UsersController {
 
-    constructor(private usersService: UsersService) {
-    }
+    constructor(private usersService: UsersService) {}
 
     @ApiOperation({
         summary: "Getting all users",
         description: "This function returns all user entities, witch does not take any params"
     })
     @ApiResponse({type: [User], status: 200})
-    @UseGuards(AuthGuard)
     @Get("/get-users")
     async getUsers(): Promise<User[]> {
         try {
@@ -34,7 +31,6 @@ export class UsersController {
     })
     @ApiResponse({type: User, status: 200})
     @Get("/get-user/:username")
-    @UseGuards(AuthGuard)
     async getUserByUsername(@Param("username") username: string): Promise<User>
     {
         try {
@@ -51,7 +47,6 @@ export class UsersController {
     })
     @ApiResponse({type: User, status: 200})
     @Get("/get-user/:email")
-    @UseGuards(AuthGuard)
     async getUserByEmail(@Param("email") email: string): Promise<User>
     {
         try {
@@ -66,28 +61,28 @@ export class UsersController {
         description: "This function returns User by GetUserProfileDto, witch takes three fields: " +
             "username, email, avatarImageUrl"
     })
-    @ApiResponse({type: User, status: 200})
     @UseGuards(AuthGuard)
+    @ApiResponse({type: User, status: 200})
     @Get("/get-user-profile")
     async getUserProfile(@Req() request): Promise<Partial<User>>
     {
         try {
-            const email = request.user.email
-            return this.usersService.getUserProfile(email)
+            const userId = request.user.id
+            return this.usersService.getUserProfile(userId)
         } catch (error) {
             throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
 
-    // @UseGuards(AuthGuard)
-    // @Delete("/delete-account")
-    // async deleteUserAccount(@Req() request): Promise<[User, Token]> {
-    //     try {
-    //         const userId = request.user.id
-    //         return this.usersService.deleteUserAccount(userId)
-    //     } catch (error) {
-    //         throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
-    //     }
-    // }
+    @UseGuards(AuthGuard)
+    @Delete("/delete-account")
+    async deleteUserAccount(@Req() request): Promise<User> {
+        try {
+            const userId = request.user.id
+            return this.usersService.deleteUserAccount(userId)
+        } catch (error) {
+            throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
+        }
+    }
 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,35 +1,30 @@
-import {BadRequestException, forwardRef, Get, Inject, Injectable, NotFoundException} from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
-import {User} from "../entities/users.entity";
+import {User} from "./users.entity";
 import {Repository} from "typeorm";
-import { Connection } from "typeorm";
-import {RegisterDto} from "../../auth/dto/register.dto";
-import {Token} from "../entities/tokens.entity";
-import {GetUserProfileDto} from "../dto/get-user-profile.dto";
-import * as stream from "node:stream";
-import {TokensService} from "./tokens.service";
+import {RegisterDto} from "../auth/dto/register.dto";
+import {GetUserProfileDto} from "./dto/get-user-profile.dto";
 
 @Injectable()
 export class UsersService {
 
     constructor(@InjectRepository(User)
-                private readonly usersRepository: Repository<User>,
-                private readonly connection: Connection) {
+                private readonly usersRepository: Repository<User>)
+    {
     }
 
     NOT_FOUND_USER_MESSAGE = "user was not found"
 
     async createUser(createUserDto: RegisterDto): Promise<User> {
-        const user = await this.usersRepository.create(createUserDto)
+        const user = this.usersRepository.create(createUserDto)
 
         return await this.usersRepository.save(user)
     }
 
     async getUserByEmail(email: string): Promise<User> {
-        const user = await this.usersRepository.findOne({
+        return  await this.usersRepository.findOne({
             where: {email: email}
         })
-        return user
     }
 
     async getUserById(id: string): Promise<User> {
@@ -66,8 +61,8 @@ export class UsersService {
         return await this.usersRepository.save(userData)
     }
 
-    async getUserProfile(userEmail: string): Promise<GetUserProfileDto> {
-        const user = await this.getUserByEmail(userEmail)
+    async getUserProfile(userId: string): Promise<GetUserProfileDto> {
+        const user = await this.getUserById(userId)
 
         if (!user) {
             throw new NotFoundException(this.NOT_FOUND_USER_MESSAGE)
@@ -80,6 +75,22 @@ export class UsersService {
         }
 
         return getUserProfileDto
+    }
+
+    async deleteUserAccount(userId: string): Promise<User> {
+
+        const user = await this.usersRepository.findOne({
+            where: { id: userId },
+            relations: [
+                "savedPlaces",
+                "token",
+                "file",
+                "chatRequests",
+                "chats"
+            ]
+        })
+
+        return await this.usersRepository.remove(user)
     }
 
 

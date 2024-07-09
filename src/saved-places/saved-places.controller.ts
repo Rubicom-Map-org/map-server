a5afbaecbddb38604415821d0bbbbd94328cc4e7
yurkenchik <yura.ilchshyn@gmail.com>
2024-07-09 23:19:48 +0300

    updated

--- src/saved-places/saved-places.controller.ts
+++ src/saved-places/saved-places.controller.ts
@@ -15,7 +15,9 @@ import {SavedPlacesService} from "./saved-places.service";
 import {SaveSaveDto} from "./dto/save-save.dto";
 import {SavedPlace} from "./saved-places.entity";
 import {AuthGuard} from "@nestjs/passport";
+import {UserId} from "../decorators/user-id.decorator";
 
+// @UseGuards(AuthGuard)
 @Controller('saved-places')
 export class SavedPlacesController {
 
@@ -24,39 +26,36 @@ export class SavedPlacesController {
 
     @UsePipes(ValidationPipe)
     @Post("/save-place")
-    async savePlace(@Req() request,
+    async savePlace(@UserId() userId: string,
                     @Body() savedPlaceDto: SaveSaveDto): Promise<SavedPlace>
     {
         try {
-            const userId = request.user.id;
             return this.savedPlacesService.savePlace(userId, savedPlaceDto);
         } catch (error) {
             if (error instanceof HttpException) throw Error
             throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
-
+    
     @Get("/:savedPlaceId")
-    async getSavedPlace(@Req() request,
+    async getSavedPlace(@UserId() userId: string,
                         @Param("savedPlaceId") savedPlaceId: string): Promise<SavedPlace> {
         try {
-            const userId = request.user.id;
             return this.savedPlacesService.getSavedPlace(userId, savedPlaceId);
         } catch (error) {
             throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
-
+    
     @Get("/")
-    async getSavedPlaces(@Req() request): Promise<SavedPlace[]> {
+    async getSavedPlaces(@UserId() userId: string): Promise<SavedPlace[]> {
         try {
-            const userId = request.user.id;
             return this.savedPlacesService.getSavedPlaces(userId)
         } catch (error) {
             throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR)
         }
     }
-
+    
     @Delete("")
     async deletePlaceFromList() {
         try {

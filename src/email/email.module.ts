bd86492da541482000b3a1578fe8f21b160d1d5d
yurkenchik <yura.ilchshyn@gmail.com>
2024-10-24 04:01:03 +0300

    accomplsihed logic for emails sending

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,43 +1,11 @@
 import { Module } from '@nestjs/common';
-import { EmailService } from './email.service';
-import {MailerModule} from "@nestjs-modules/mailer";
-import { EmailController } from './email.controller';
-import * as dotenv from "dotenv"
-import * as process from "node:process";
-import {UsersModule} from "../users/users.module";
-import {JwtModule} from "@nestjs/jwt";
-import {AuthModule} from "../auth/auth.module";
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EmailService } from './services/email.service';
+import { ConfigModule } from '@nestjs/config';
+import { SendgridClientService } from './services/send-grid.service';
 
 @Module({
-  providers: [EmailService],
-  imports: [
-      ConfigModule,
-      MailerModule.forRootAsync({
-          imports: [ConfigModule],
-          inject: [ConfigService],
-          useFactory: (configService: ConfigService) => ({
-              transport: {
-                  host: configService.get<string>("MAIL_HOST"),
-                  port: configService.get<number>("MAIL_PORT"),
-                  secure: false,
-                  auth: {
-                     user: configService.get<string>("MAIL_USER"),
-                      pass: configService.get<string>("MAIL_PASSWORD")
-                  },
-              },
-              defaults: {
-                  from: '"No Reply" <noreply@example.com>',
-              },
-              options: {
-                  strict: Boolean(configService.get<string>("MAIL_OPTIONS_STRICT"))
-              }
-          })
-      }),
-      UsersModule,
-      JwtModule
-  ],
-  exports: [EmailService],
-  controllers: [EmailController]
+  providers: [EmailService, SendgridClientService],
+  imports: [ConfigModule],
+  exports: [EmailService, SendgridClientService],
 })
 export class EmailModule {}
--- src/email/services/email.service.ts
+++ src/email/services/email.service.ts
@@ -1,47 +1,26 @@
-import {Injectable, InternalServerErrorException, NotFoundException} from '@nestjs/common';
-import {MailerService} from "@nestjs-modules/mailer";
-import * as process from "node:process";
-import {UsersService} from "../users/users.service";
-import {ExceptionMessage} from "../utils/exception-message.enum";
-import { EmailRepository } from './email-repository.abstract';
+import {Injectable} from '@nestjs/common';
+import { EmailRepository } from '../email-repository.abstract';
 import { ConfigService } from '@nestjs/config';
+import { SendgridClientService } from './send-grid.service';
+import { MailDataRequired } from '@sendgrid/mail';
 
 @Injectable()
 export class EmailService extends EmailRepository {
     
     constructor(
-        private readonly mailerService: MailerService,
-        private readonly userService: UsersService,
         private readonly configService: ConfigService,
+        private readonly sendgridClientService: SendgridClientService
     ) {
         super();
     }
 
-    async sendMessageToOwners(email: string, message: string) {
-        try {
-            await this.mailerService.sendMail({
-                from: email,
-                to: process.env.MAIL_USER,
-                subject: message,
-            })
-        } catch (error) {
-            throw new InternalServerErrorException(error.message);
-        }
-    }
-    
-    async sendVerificationCodeByEmail(email: string): Promise<void> {
-        try {
-            const user = await this.userService.getUserByEmail(email);
-            if (!user) throw new NotFoundException(ExceptionMessage.USER_NOT_FOUND);
-
-            const verificationCode = Math.floor(100000 + Math.random() * 900000).toString();
-            await this.mailerService.sendMail({
-                from: this.configService.get<string>("MAIL_SENDER"),
-                to: email,
-                subject: `Your verification code is: ${verificationCode}`
-            })
-        } catch (error) {
-            throw new InternalServerErrorException(error.message);
-        }
+    async sendVerificationCodeByEmail(email: string, verificationCode: string): Promise<void> {
+        const mail: MailDataRequired = {
+            to: email,
+            from: this.configService.get<string>("EMAIL_SENDER"),
+            subject: "Code verification",
+            text: `Your verification code: ${verificationCode}`
+        };
+        await this.sendgridClientService.sendEmail(mail);
     }
 }
